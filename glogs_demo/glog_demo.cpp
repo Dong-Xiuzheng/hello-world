36278c2b8a60c9011ed70e2371fa9d825f913983
Dong-Xiuzheng <dxz00ww@163.com>
2019-05-25 15:23:06 +0800

    add

--- glogs_demo/glog_demo.cpp
+++ glogs_demo/glog_demo.cpp
@@ -1,13 +1,12 @@
 #include <glog/logging.h>  
-//#include<gflags/flags.h>  
+#include<gflags/gflags.h>  
 int main(int argc,char* argv[]) {  
-    // 要使用下面的api，需要安装额外的gflags,以及添加上面注释的头文件   
-    // google::ParseCommandLineFlags(&argc, &argv, true);  
+     google::ParseCommandLineFlags(&argc, &argv, true);  
 
-    // Initialize Google's logging library.  
+    //Initialize Google's logging library.  
     google::InitGoogleLogging(argv[0]);  
 
-  //需要先在本目录下先建立有个名为“log”的文件夹,否则会报错
+    //需要先在本目录下先建立有个名为“log”的文件夹,否则会报错
     FLAGS_log_dir = "./log";
     //or  google::SetLogDestination(google::GLOG_INFO, "./log_");  
 
